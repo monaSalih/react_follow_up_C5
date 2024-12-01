8e50632269231bafaac335bbd5dd27423d988ed1
Mona Salih <monaalobisat@gmail.com>
2024-12-01 10:39:45 +0300

    update

--- src/component/TodoForm.js
+++ src/component/TodoForm.js
@@ -2,12 +2,14 @@ import React, {useState}  from "react";
 // import Form from 'react-bootstrap/Form';
 // import InputGroup from 'react-bootstrap/InputGroup';
 // import 
-export const TodoForm = () => {
+export const TodoForm = ({addtodo}) => {
   const [value,setValue]=useState("");
     const handleSubmit=e=>{
       e.preventDefault();
       setValue("");
-      console.log(value,"form value");
+      addtodo(value);
+
+      // console.log(value,"form value");
     }
   return (
     <form className="TodoForm" onSubmit={handleSubmit}>
--- src/component/TodoWraper.js
+++ src/component/TodoWraper.js
@@ -2,13 +2,20 @@ import React, { useState } from "react";
 
 import { TodoForm } from "./TodoForm";
 export const TodoWraper = () => {
+    const [todos,setTodos]=useState([])
+
+    const addtodo=(todo)=>{
+        setTodos([...todos,{banana:todo}])
+        console.log(todos,"todos result");
+        
+    }
   return (
     <div className="">
       <h1 className="text-center">To Do List</h1>
       <div className="container vh-100 d-flex justify-content-center align-items-center">
         <div className="row w-100">
           <div className="col-md-6 mx-auto">
-           <TodoForm/>
+           <TodoForm addtodo={addtodo} />
           </div>
         </div>
       </div>
